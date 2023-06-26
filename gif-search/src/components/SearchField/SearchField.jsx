f35599349dc4502aedb21b3f79cbb92e1b000102
eluzung <luzungelaine@gmail.com>
2023-06-26 07:59:09 -0400

    Add filter search results by rating

--- gif-search/src/components/SearchField/SearchField.jsx
+++ gif-search/src/components/SearchField/SearchField.jsx
@@ -8,9 +8,14 @@ function Search(props) {
     event.preventDefault();
 
     const term = props.searchTerm;
+    const rating = props.rating;
 
-    props.getSearch(term);
+    console.log(term);
+    console.log(rating);
+
+    props.getSearch(term, rating);
     props.setSearchTerm("");
+    props.setRating("");
   }
 
   return (
@@ -18,12 +23,56 @@ function Search(props) {
       <form onSubmit={handleSearch} className="form">
         <label>Search for any GIF!</label>
         <input
+          className="search"
           type="text"
           placeholder="type anything..."
           value={props.searchTerm}
           onChange={(event) => props.setSearchTerm(event.target.value)}
         />
         <button type="submit">Submit</button>
+        <div className="filter">
+          <h3>Filter your search by selecting a rating!</h3>
+          <div>
+            <input
+              className="rating"
+              type="checkbox"
+              id="g"
+              value="g"
+              onChange={(event) => props.setRating(event.target.value)}
+            />
+            <label for="g">G</label>
+          </div>
+          <div>
+            <input
+              className="rating"
+              type="checkbox"
+              id="pg"
+              value="pg"
+              onChange={(event) => props.setRating(event.target.value)}
+            />
+            <label for="pg">PG</label>
+          </div>
+          <div>
+            <input
+              className="rating"
+              type="checkbox"
+              id="pg-13"
+              value="pg-13"
+              onChange={(event) => props.setRating(event.target.value)}
+            />
+            <label for="pg-13">PG-13</label>
+          </div>
+          <div>
+            <input
+              className="rating"
+              type="checkbox"
+              id="r"
+              value="r"
+              onChange={(event) => props.setRating(event.target.value)}
+            />
+            <label for="r">R</label>
+          </div>
+        </div>
       </form>
       <GifCard gifArray={props.searchArray} />
     </div>
